c88e627f6620482b2b2e864a96adcfcd48d0740a
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2023-07-28 18:10:22 +0530

    Add main and temp fields to index.ejs to display weather data and temperature. Make some changes to app.js too, in order to send request to OpenWeather server and get the weather data.

--- app.js
+++ app.js
@@ -7,14 +7,19 @@ const _ = require("lodash");
 const https = require("node:https")
 app.use(express.static("public"))
 app.set("view engine", "ejs");
+const {log} = require("console");
 
 //API KEY - 75a7405c19ea0994ec4e4b82b78e39a2
-
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=27.1300&lon=81.9300&appid=75a7405c19ea0994ec4e4b82b78e39a2";
 app.get("/", (req, res) => {
-    const data = {
-        check : "Check",
-    }
-    res.render("index", data);
+    https.get(url, (res) => {
+        res.on("data", (data) => {
+            const weatherData = JSON.parse(data);
+            log("StatusCode:", res.statusCode);
+            log(weatherData);
+        })
+    })
+
 })
 
 app.listen(3000, () => {
