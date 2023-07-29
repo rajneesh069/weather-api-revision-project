7173d6727fb04ff539e87aab1856062beea669e6
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2023-07-29 11:04:18 +0530

    Add home.ejs page to ask country code and city name from the user as an input. Then add a post route in the app.js file to handle the input data. After that app.js renders index.ejs to show weather description and icon related to the data received.

--- app.js
+++ app.js
@@ -10,20 +10,36 @@ app.set("view engine", "ejs");
 const { log } = require("console");
 
 //API KEY - 75a7405c19ea0994ec4e4b82b78e39a2
-const url = "https://api.openweathermap.org/data/2.5/weather?units=metric&lat=27.1300&lon=81.9300&appid=75a7405c19ea0994ec4e4b82b78e39a2";
+
 app.get("/", (req, res) => {
+    res.render("home");
+})
+
+app.post("/", (req, res) => {
+    const city = req.body.city;
+    const country = req.body.country;
+    const url = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "," + country + "&appid=75a7405c19ea0994ec4e4b82b78e39a2";
     https.get(url, (response) => {
         response.on("data", (data) => {
-            const weatherData = JSON.parse(data);
-            const renderData = {
-                icon : weatherData.weather[0].icon,
-                main: weatherData.weather[0].main,
-                temp: weatherData.main.temp,
-            }
-            res.render("index", renderData);
+            const geocoderData = JSON.parse(data);
+            const lon = geocoderData[0].lon;
+            const lat = geocoderData[0].lat;
+            console.log(lon, lat);
+            const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&lat=" + lat + "&lon=" + lon + "&appid=75a7405c19ea0994ec4e4b82b78e39a2";
+            https.get(weatherUrl, (response) => {
+                response.on("data", (data) => {
+                    const weatherData = JSON.parse(data);
+                    const renderData = {
+                        icon: weatherData.weather[0].icon,
+                        main: weatherData.weather[0].main,
+                        temp: weatherData.main.temp,
+                    }
+                    res.render("index", renderData);
+                })
+            })
+
         })
     })
-
 })
 
 app.listen(3000, () => {
