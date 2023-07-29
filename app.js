00da739d4946ad85331633ca4291a8756c4f8927
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2023-07-29 10:42:54 +0530

    Add a README file to the project

--- app.js
+++ app.js
@@ -7,16 +7,20 @@ const _ = require("lodash");
 const https = require("node:https")
 app.use(express.static("public"))
 app.set("view engine", "ejs");
-const {log} = require("console");
+const { log } = require("console");
 
 //API KEY - 75a7405c19ea0994ec4e4b82b78e39a2
-const url = "https://api.openweathermap.org/data/2.5/weather?lat=27.1300&lon=81.9300&appid=75a7405c19ea0994ec4e4b82b78e39a2";
+const url = "https://api.openweathermap.org/data/2.5/weather?units=metric&lat=27.1300&lon=81.9300&appid=75a7405c19ea0994ec4e4b82b78e39a2";
 app.get("/", (req, res) => {
-    https.get(url, (res) => {
-        res.on("data", (data) => {
+    https.get(url, (response) => {
+        response.on("data", (data) => {
             const weatherData = JSON.parse(data);
-            log("StatusCode:", res.statusCode);
-            log(weatherData);
+            const renderData = {
+                icon : weatherData.weather[0].icon,
+                main: weatherData.weather[0].main,
+                temp: weatherData.main.temp,
+            }
+            res.render("index", renderData);
         })
     })
 
